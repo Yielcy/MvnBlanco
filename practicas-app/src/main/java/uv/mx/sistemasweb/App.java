2a49d91b35cb17e4f0e2c4bcabca0c7b96c91f6d
Yielcy <jonthan.galio108@gmail.com>
2021-12-13 14:26:30 -0600

    Jonathan Contrereras Calificación

--- practicas-app/src/main/java/uv/mx/sistemasweb/App.java
+++ practicas-app/src/main/java/uv/mx/sistemasweb/App.java
@@ -1,5 +1,7 @@
 package uv.mx.sistemasweb;
 
+import static spark.Spark.*;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        staticFiles.location("/aprobado");
+        get("/aprobado", (req, res) -> "Calificación: 9");
     }
+
+
 }
